f18b70f9cabd4bdda9f9b8d52c18c86e89c77aae
guiworm <dylan.mccrindle@gmail.com>
2016-11-06 21:16:39 -0500

    Commit Vehicles

---
+++ Vehicles/Vehicles/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Vehicles
+//
+//  Created by Transferred on 9/8/13.
+//  Copyright (c) 2013 Designated Nerd Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Vehicle.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
