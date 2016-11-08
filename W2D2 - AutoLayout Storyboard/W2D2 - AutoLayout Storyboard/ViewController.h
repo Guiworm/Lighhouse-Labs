150393512a125b95e033d04edcb0370215607ec1
guiworm <dylan.mccrindle@gmail.com>
2016-11-08 16:42:34 -0500

    Done

---
+++ W2D2 - AutoLayout Storyboard/W2D2 - AutoLayout Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W2D2 - AutoLayout Storyboard
+//
+//  Created by Dylan McCrindle on 2016-11-08.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ W2D2 - AutoLayout Storyboard/W2D2 - AutoLayout Storyboard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  W2D2 - AutoLayout Storyboard
+//
+//  Created by Dylan McCrindle on 2016-11-08.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
