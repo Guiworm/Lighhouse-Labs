b621801095001d2b368bd9651007881851cdfb09
guiworm <dylan.mccrindle@gmail.com>
2016-11-11 10:46:37 -0500

    Created project and finished User Story 1

---
+++ Tip Calculator/Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Dylan McCrindle on 2016-11-11.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ W2D4 - Gestures/W2D4 - Gestures/PanViewController.h
@@ -0,0 +1,13 @@
+//
+//  PanViewController.h
+//  W2D4 - Gestures
+//
+//  Created by Dylan McCrindle on 2016-11-10.
+//  Copyright © 2016 Dylan McCrindle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface PanViewController : ViewController
+
+@end
